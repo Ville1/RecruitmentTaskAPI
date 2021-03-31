d2e31ffe058034eeae170fcfc6026a957ed2ed46
Ville1 <villejmyllykangas@gmail.com>
2021-03-31 15:55:14 +0300

    Getting today's emails in csv

--- RecruitmentTaskAPI/Controllers/EmailController.cs
+++ RecruitmentTaskAPI/Controllers/EmailController.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using RecruitmentTaskAPI.Data;
+using RecruitmentTaskAPI.Data.DB.Model;
 using RecruitmentTaskAPI.Data.Http;
+using RecruitmentTaskAPI.Data.Repository;
 using RecruitmentTaskAPI.Utils;
 
 namespace RecruitmentTaskAPI.Controllers
@@ -26,9 +31,37 @@ namespace RecruitmentTaskAPI.Controllers
             try {
                 FileManager.Save(input);
             } catch (Exception exception) {
+                Logger.LogException(exception);
                 return new EmailAddResponse(Response, HttpStatusCode.InternalServerError, exception.Message, false);
             }
             return new EmailAddResponse(Response, HttpStatusCode.OK, true);
+        }
+
+        [HttpGet]
+        [Route("getEmailsToday")]
+        [Produces("application/json")]
+        public GetEmailsResponse getEmailsToday()
+        {
+            try {
+                List<EmailModel> emails = Repository.Emails.GetAll().Where(x =>
+                    x.Created != null &&
+                    x.Created.Value.Year == DateTime.Now.Year &&
+                    x.Created.Value.Month == DateTime.Now.Month &&
+                    x.Created.Value.Day == DateTime.Now.Day
+                ).ToList();
+                StringBuilder csv = new StringBuilder();
+
+                char separator = ';';
+                foreach(EmailModel email in emails) {
+                    csv.Append(email.EmailKey).Append(separator).Append(email.Email).Append(separator).Append(string.Join(',', email.Attributes.Select(x => x.Name).ToArray()))
+                        .Append(separator).Append(Environment.NewLine);
+                }
+
+                return new GetEmailsResponse(null, csv.ToString());
+            } catch(Exception exception) {
+                Logger.LogException(exception);
+                return new GetEmailsResponse(Response, HttpStatusCode.InternalServerError, exception.Message, null);
+            }
         }
     }
 }
---
+++ RecruitmentTaskAPI/Data/Http/GetEmailsResponse.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Net;
+
+namespace RecruitmentTaskAPI.Data.Http
+{
+    public class GetEmailsResponse : Response
+    {
+        public string Csv { get; set; }
+
+        public GetEmailsResponse(string message, string csv) : base(message)
+        {
+            Csv = csv;
+        }
+
+        public GetEmailsResponse(HttpResponse response, HttpStatusCode code, string csv) : base(response, code)
+        {
+            Csv = csv;
+        }
+
+        public GetEmailsResponse(HttpResponse response, HttpStatusCode code, string message, string csv) : base(response, code, message)
+        {
+            Csv = csv;
+        }
+    }
+}
